1806bd613420c50bda82e1d9fae25a654e3c84ba
monokaijs <monokaijs@gmail.com>
2022-06-04 17:20:53 +0700

    Fix ts error

--- layouts/AppLayout.tsx
+++ layouts/AppLayout.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import {Header} from "../components/Header";
 import {Layout} from "antd";
 
-export const AppLayout = ({children}) => {
+export interface AppLayoutProps {
+  children: any
+}
+
+export const AppLayout = ({children}: AppLayoutProps) => {
   return (
     <>
 
